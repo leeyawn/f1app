33678192a0971646ae73e832628dec6b0df63351
Leon Letournel <lletourlt@sunypoly.edu>
2025-03-25 14:21:50 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-center text-lg text-blue-400">letournel.dev</h1>
+  )
+}
+
+export default App
